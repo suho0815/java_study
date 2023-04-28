5d42ab8c8c7c290caf572b8b4bf97ccc358ae341
suho <suho0815@naver.com>
2023-04-28 17:56:47 +0900

    level04_02

--- src/backjoon/level04/level04_02.java
+++ src/backjoon/level04/level04_02.java
@@ -2,7 +2,7 @@ package backjoon.level04;
 
 import java.util.*;
 
-public class level04_01 {
+public class level04_02 {
 	
 	public static void main(String[] args) {
 		
@@ -10,19 +10,23 @@ public class level04_01 {
 		
 		int n = sc.nextInt();
 		int []a = new int[n];
-		int s =0;
+		int max = 0;
+		int min = 0;
+		
 		for(int i = 0; i < n; i++) {
 			a[i] = sc.nextInt();
 		}
 		
-		int v = sc.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			if (a[i] == v) {
-				s++;
+		for(int i = 0; i < n - 1; i++) {
+			if (a[i] < a[i+1]) {
+				min = a[i];
+				max = a[i+1];
+			}else {
+				min = a[i+1];
+				max = a[i];
 			}
 		}
-		System.out.println(s);
+		System.out.println(min + " "+ max);
 		
 	}
 }
