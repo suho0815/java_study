b5b27ea656f5e8300be7229a88697be2ebdaf99b
suho <suho0815@naver.com>
2023-05-19 18:01:09 +0900

    별

--- src/study03/별찍기10.java
+++ src/study03/별찍기10.java
@@ -3,10 +3,15 @@ package study03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 
 public class 별찍기10 {
 
-	static void star() {
+	static void star(int n) {
+		
+		System.out.print("***");
+		System.out.print("* *");
+		System.out.print("***");
 		
 	}
 	
@@ -14,7 +19,7 @@ public class 별찍기10 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-
+		
 	}
 
 }
