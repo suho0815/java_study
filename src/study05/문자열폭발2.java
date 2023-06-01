4fa3c17a0278174b119ef9187b6224a28bce5b08
suho0815 <50311505+suho0815@users.noreply.github.com>
2023-06-02 05:11:26 +0900

    문자열폭발,,,,,,,,,,,,,ㅠㅠ

--- src/study05/문자열폭발2.java
+++ src/study05/문자열폭발2.java
@@ -10,28 +10,26 @@ public class 문자열폭발2 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Stack<Character> stack = new Stack<>();
-		//StringBuilder sb = new StringBuilder();
+		Stack<Character> stack = new Stack<Character>();
 		String str = br.readLine();
 		String boom = br.readLine();
 		
-		br.close();
-		
-		//int p = 0;
 		int boom_len = boom.length();
 		int str_len = str.length();
 		
 		for (int i = 0; i < str_len; i++) {
 			stack.push(str.charAt(i));
 			int st_size = stack.size();
+			
 			if(st_size >= boom_len) {
-				int cnt = 0;
+				boolean fire = true;
 				for(int j = 0; j < boom_len; j++) { //boom 인지 확인
-					if(stack.get(st_size - boom_len + j) == boom.charAt(j)) {
-						cnt++;
+					if(stack.get(st_size - boom_len + j) != boom.charAt(j)) {
+						fire = false;
+						break;
 					}
 				}
-				if(cnt == boom_len) { //boom 이면 pop
+				if(fire) { //boom 이면 pop
 					for(int j = 0; j < boom_len; j++) {
 						stack.pop();
 					}
@@ -44,10 +42,9 @@ public class 문자열폭발2 {
 			System.out.println("FRULA");
 			return;
 		}
-		String result = "";
-		while (!stack.isEmpty()) {
-			//sb.insert(0, stack.pop());
-			result = stack.pop() + result;
+		StringBuilder result = new StringBuilder();
+		for(char c : stack ) {
+			result.append(c);
 		}
 		System.out.println(result);
 		
