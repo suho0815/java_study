24ad50309435d5dfa01bc5c235eb405978b33df3
suho <suho0815@naver.com>
2023-06-08 18:00:19 +0900

    별 찍기

---
+++ src/study06/별찍기11.java
@@ -0,0 +1,28 @@
+package study06;
+
+import java.util.Scanner;
+
+public class 별찍기11 {
+	static char[][] arr;
+	
+	public static void star() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		arr = new char[n][n*2];
+		
+		/*
+		 세로 : 3 X 2^k = n
+		 가로 : 3 X 2^k+1 = n*2
+		*/
+		
+		
+		
+		
+	}
+
+}
