e3a6a3a997d0e1cdec146d2377c3b8d4d534c2be
suho <suho0815@naver.com>
2023-06-09 17:48:38 +0900

    별찍기11

--- src/study06/별찍기11_2.java
+++ src/study06/별찍기11_2.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
-public class 별찍기11 {
-	static char[][] arr;
+public class 별찍기11_2 {
+	static StringBuilder[] sb ;
 	static char[][] star = {
 			{' ',' ','*',' ',' '},
 			{' ','*',' ','*',' '},
@@ -17,47 +17,36 @@ public class 별찍기11 {
 		if (n == 3) {
 			for(int i = 0; i <star.length; i++) {
 				for(int j = 0; j <star[0].length; j++) {
-					arr[y+i][x+j] = star[i][j];
+					sb[y+i].setCharAt(x+j, star[i][j]);
 				}
 			}
+			
 			return;
 		}
+		
 		int size = n/2;
 		star(y, x+size, size);
 		star(y+size, x, size);
 		star(y+size, x+n, size);
+		
 	}
 	
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		int n = sc.nextInt();
-		arr = new char[n][n*2];
 		
-		for(int i = 0; i < arr.length; i++) {
-			for( int j = 0; j < arr[0].length; j++ ) {
-				arr[i][j] = ' ';
-			}
+		sb = new StringBuilder[n];
+		for(int i = 0; i < n; i++) {
+			sb[i] = new StringBuilder(String.format("%"+ (n*2) +"s", ' '));
 		}
-		
 		/*
 		 세로 : 3 X 2^k = n
 		 가로 : 3 X 2^k+1 = n*2
 		*/
 		star(0 , 0, n);
-		for(int i = 0; i< arr.length; i++) {
-			bw.write(arr[i]);
-			bw.write("\n");
+		for(int i = 0; i< n; i++) {
+			System.out.println(sb[i]);
 		}
-//		for(int i = 0; i< arr.length; i++) {
-//			for(int j = 0; j <arr[0].length; j++) {
-//				bw.write(arr[i][j]);
-//			}
-//			bw.write("\n");
-//		}
-		bw.flush();
-		bw.close();
 		
 	}
 
