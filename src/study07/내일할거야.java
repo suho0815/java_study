c39e9bd78b3bcd033fef5322ce3538c768d45cb1
suho0815 <50311505+suho0815@users.noreply.github.com>
2023-06-14 04:28:45 +0900

    내일할거야

---
+++ src/study07/내일할거야.java
@@ -0,0 +1,70 @@
+package study07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class homework {
+	int d, t;
+	int start_day, end_day; //과제 시작한 날, 과제 끝낸 날
+	
+	public homework(int d, int t) {
+		this.d = d;
+		this.t = t;
+	}
+
+}
+
+public class 내일할거야 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		homework[] hws = new homework[n]; 
+		StringTokenizer st;
+		for(int i =0; i< n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int d = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			
+			hws[i] = new homework(d, t);
+		}
+		
+		// 마감 일 기준으로 내림차순 정렬
+		Comparator<homework> ch = new Comparator<homework>() {
+			@Override
+			public int compare(homework o1, homework o2) {
+				return o1.t - o2.t;
+			}
+		};
+		Arrays.sort(hws, ch.reversed());
+		
+		int day = 0; //며칠 동안 놀 수 있음? 
+		hws[0].start_day = hws[0].t - hws[0].d + 1;
+		hws[0].end_day = hws[0].t;
+		for(int i = 1; i < n; i++) {
+			// 이전과제 마감일과 현재과제 시작일이랑 겹치는 경우
+			if(hws[i-1].start_day <= hws[i].t) {
+				hws[i].end_day = hws[i-1].start_day -1;
+				hws[i].start_day = hws[i].end_day - hws[i].d + 1;
+			}else {
+				hws[i].start_day = hws[i].t - hws[i].d + 1;
+				hws[i].end_day = hws[i].t;
+			}
+			
+			if(day < (hws[i-1].start_day-1) - hws[i].end_day)
+				day = (hws[i-1].start_day-1) - hws[i].end_day;
+		}
+		
+		if(day < hws[n-1].start_day -1)
+			day = hws[n-1].start_day -1;
+		
+		System.out.println(day);
+		
+	}
+}
