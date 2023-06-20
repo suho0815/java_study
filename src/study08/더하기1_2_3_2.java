448b22bf9c744d88652af42797282608e4bf1b74
suho0815 <50311505+suho0815@users.noreply.github.com>
2023-06-21 01:45:00 +0900

    study08

--- src/study08/더하기1_2_3_2.java
+++ src/study08/더하기1_2_3_2.java
@@ -4,41 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 더하기1_2_3 {
-	static int[] result;
-	
-	public static int plus(int n) {
+public class 더하기1_2_3_2 {
 
-		if(result[n] != 0)
-			return result[n];
-		
-		if(n-3 >= 0) {
-			result[n-1] = plus(n-1);
-			result[n-2] = plus(n-2);
-			result[n-3] = plus(n-3);
-		}
-		result[n] = result[n-1] + result[n-2] + result[n-3]; 
-		
-		return result[n];
-	}
-	
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		//각 테스트 케이스마다, n을 1, 2, 3의 합으로 나타내는 방법의 수를 출력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int t = Integer.parseInt(br.readLine());
-		result = new int[11];
+		
+		int[] tmp = new int[t]; // 입력값을 저장할 배열
+		int max = 0; // 반복 횟수를 줄이기 위해 테스트케이스 중 최대값을 구함
+		for(int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+			tmp[i] = n;
+			if(max < n) {
+				max = n;
+			}
+		}
+		
+		int[] result = new int[max];
 		result[0] = 1;
 		result[1] = 2;
 		result[2] = 4;
 		
-		for(int i = 0; i < t; i++) {
-			int n = Integer.parseInt(br.readLine());
-			
-			System.out.println(plus(n-1));
+		for(int i = 3; i < max; i++) {
+			result[i] = result[i -1] + result[i -2] + result[i -3];
 		}
-		
+		for(int i = 0; i < t; i++) {
+			System.out.println(result[tmp[i] - 1]);
+		}
 		
 	}
 
