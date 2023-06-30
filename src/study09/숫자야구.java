79576be570d5982445157225c5ec569876bce3d3
suho <suho0815@naver.com>
2023-06-30 18:01:24 +0900

    숫자야구

---
+++ src/study09/숫자야구.java
@@ -0,0 +1,21 @@
+package study09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 숫자야구 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			
+		}
+		
+	}
+}
